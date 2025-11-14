5ad11aa03c905b45b221397f7e0cba9cf05fa76e
Nick Schneble <nschneble@users.noreply.github.com>
2025-11-13 22:08:40 -0500

    scaffold user controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  passwordless_for :users
+
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,6 +15,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  passwordless_for :users
 end
