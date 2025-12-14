062336df31e0e2f36c491d53317c6e886fcd1045
Nick Schneble <nschneble@users.noreply.github.com>
2025-12-14 00:41:20 -0500

    ditch sign in alerts when you switch pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < Passwordless::SessionsController
+  after_action -> { flash.discard(:alert) }
+
   def create
     User.find_or_create_by(email: normalized_email_param) do |user|
       puts "Creating new user for email `#{user.email}`"
