56e432d0c3974d18b364f01dc0f07fadf9b2f3bc
Nick Schneble <nschneble@users.noreply.github.com>
2025-12-14 12:18:41 -0500

    prevent redundant sign in attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < Passwordless::SessionsController
+  before_action :no_users_allowed!, only: %i[new show]
   after_action -> { flash.discard(:alert) }
+
+  def new
+    super
+  end
 
   def create
     User.find_or_create_by(email: normalized_email_param) do |user|
@@ -7,5 +12,16 @@ class SessionsController < Passwordless::SessionsController
     end
 
     super
+  end
+
+  def show
+    super
+  end
+
+  private
+
+  def no_users_allowed!
+    return unless current_user
+    redirect_to("/", notice: "You're already signed in, sillypants")
   end
 end
