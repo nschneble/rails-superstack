5e306bdc9ac00059738d59f191b1733c28af4358
Nick Schneble <nschneble@users.noreply.github.com>
2026-01-17 13:18:36 -0500

    explicitly load Resque scheduler

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,8 +6,10 @@ require "resque/scheduler/server"
 redis_url = ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0")
 Resque.redis = redis_url
 
-# loads the schedule for resque-scheduler
-schedule_file = Rails.root.join("config", "resque_schedule.yml")
-if File.exist?(schedule_file)
-  Resque.schedule = YAML.safe_load(File.read(schedule_file), aliases: true)
+# loads the schedule for resque-scheduler only when explicitly enabled
+if ENV["RESQUE_SCHEDULE_ENABLED"] == "1"
+  schedule_file = Rails.root.join("config", "resque_schedule.yml")
+  if File.exist?(schedule_file)
+    Resque.schedule = YAML.safe_load(File.read(schedule_file), aliases: true)
+  end
 end
