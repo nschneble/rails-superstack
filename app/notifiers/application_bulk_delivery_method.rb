02d81fa8382d4d9613d3b55ee0bab368905a426a
Nick Schneble <nschneble@users.noreply.github.com>
2026-03-04 23:41:24 -0500

    exfoliate dead files

--- app/notifiers/application_bulk_delivery_method.rb
+++
@@ -1,2 +0,0 @@
-class ApplicationBulkDeliveryMethod < Noticed::BulkDeliveryMethod
-end
--- config/routes/test/demo/pages.rb
+++
@@ -1,5 +0,0 @@
-namespace :demo do
-  get "api", to: "pages#api", as: :api
-  get "terminal", to: "pages#terminal", as: :terminal
-  get "welcome", to: "pages#welcome", as: :welcome
-end
