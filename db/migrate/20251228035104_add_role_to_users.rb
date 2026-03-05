926da4074c85a6b5446b3f8dc6da65bac50a01ae
Nick Schneble <nschneble@users.noreply.github.com>
2026-03-05 00:19:56 -0500

    squash migrations

--- db/migrate/20251228035104_add_role_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddRoleToUsers < ActiveRecord::Migration[8.1]
-  disable_ddl_transaction!
-
-  def change
-    add_column :users, :role, :integer, null: false, default: 0
-    add_index :users, :role, algorithm: :concurrently
-  end
-end
