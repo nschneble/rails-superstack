442a7caa911b23cb4d049752a6a8e4cca9115566
Nick Schneble <nschneble@users.noreply.github.com>
2026-03-04 23:55:39 -0500

    MacGuffin search upgrades

---
+++ app/components/demo/visibility_component.rb
@@ -0,0 +1,28 @@
+class Demo::VisibilityComponent < Pom::Component
+  option :visibility, required: true
+  option :palette, enums: [ :default ], default: :default
+
+  define_styles(
+    base: "absolute bottom-2 inline-flex items-center mt-4 px-2 py-1 rounded-md select-none",
+    palette: {
+      default: "bg-slate-100 text-slate-500 text-xs font-medium ring-1 ring-slate-200"
+    }
+  )
+
+  def call
+    content_tag :span, **html_options do
+      safe_join([ helpers.fas_icon("eye"), content_tag(:span, visibility, class: "ml-1 capitalize") ])
+    end
+  end
+
+  private
+
+  def html_options
+    merge_options(
+      {
+        class: styles_for(palette: palette)
+      },
+      extra_options
+    )
+  end
+end
