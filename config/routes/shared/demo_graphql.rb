2d227da87cd250da806ad39693d8d79264122d65
Nick Schneble <nschneble@users.noreply.github.com>
2026-03-06 15:28:09 -0500

    drop duplicate GraphQL routes

--- config/routes/shared/demo_graphql.rb
+++
@@ -1,4 +0,0 @@
-# demo GraphQL schema and endpoints
-get  "/demo/graphiql",         to: "graphql/demo/mac_guffins#graphiql"
-get  "/demo/graphql/describe", to: "graphql/demo/mac_guffins#describe"
-post "/demo/graphql",          to: "graphql/demo/mac_guffins#execute"
