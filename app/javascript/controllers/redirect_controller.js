1c547db00cede2d52213aa391ef33b33f9bde5ab
Nick Schneble <nschneble@users.noreply.github.com>
2026-03-23 00:19:24 -0400

    reorganize Stimulus controllers

--- app/javascript/controllers/redirect_controller.js
+++ app/javascript/controllers/redirect_controller.js
@@ -1,15 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
+// connects to data-controller="redirect"
 export default class extends Controller {
-  static values = { url: String, delay: { type: Number, default: 5000 } }
+  static values = {
+    url: String,
+    delay: { type: Number, default: 5000 },
+  };
 
   connect() {
     this.timer = setTimeout(() => {
-      window.location.href = this.urlValue
-    }, this.delayValue)
+      window.location.href = this.urlValue;
+    }, this.delayValue);
   }
 
   disconnect() {
-    clearTimeout(this.timer)
+    clearTimeout(this.timer);
   }
 }
