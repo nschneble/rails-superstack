518bc2b4d0de637097f26b010bd54cfc594a799e
Nick Schneble <nschneble@users.noreply.github.com>
2026-03-29 00:59:30 -0400

    smooth out the subscription abilities

--- lib/abilities/billing/subscriptions.rb
+++ lib/abilities/billing/subscriptions.rb
@@ -2,10 +2,11 @@ module Abilities::Billing::Subscriptions
   def self.apply(ability, user)
     if user.present?
       ability.can :manage, Subscription, user: user
-      ability.can :access, :pro_features if user.subscription&.active? && user.subscription&.pro?
     end
 
-    ability.can :manage, Subscription if user&.admin?
+    if user&.admin?
+      ability.can :manage, Subscription
+    end
   end
 end
 
