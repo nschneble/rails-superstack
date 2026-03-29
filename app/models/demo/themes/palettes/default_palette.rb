f495d848d2a80f10c20103e99ed9271c82928b46
Nick Schneble <nschneble@users.noreply.github.com>
2026-03-29 09:13:54 -0400

    fix module naming mismatch

--- app/models/demo/themes/palettes/default_palette.rb
+++ app/models/demo/themes/palettes/default_palette.rb
@@ -1,6 +1,7 @@
-module Demo::Palettes
+module Demo::Themes::Palettes
   DefaultPalette = Palette.new(
     bgr: "bg-blue-900!",
+    bdr: "ring-emerald-400/80!",
     txt: "text-white!",
     btn: "bg-slate-800!",
     lbl: "text-white!",
