cf1d2a682537c86730ffb065acae8eaf03e7f778
Nick Schneble <nschneble@users.noreply.github.com>
2026-04-04 22:52:38 -0400

    use current theme on welcome items

--- app/models/demo/themes/palettes/default_palette.rb
+++ app/models/demo/themes/palettes/default_palette.rb
@@ -1,10 +1,10 @@
 module Demo::Themes::Palettes
   DefaultPalette = Palette.new(
-    background:  "bg-slate-500!",
-    border:      "ring-amber-400/80!",
+    background:  "bg-blue-900!",
+    border:      "ring-lime-500/80!",
     text:        "text-white!",
     button:      "bg-slate-800!",
     button_text: "text-white!",
-    highlight:   "text-amber-400!"
+    highlight:   "text-lime-500!"
   )
 end
