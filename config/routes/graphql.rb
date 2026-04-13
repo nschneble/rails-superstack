acb1961a2034e1cff12429889fa1b2c1e62f1f29
Nick Schneble <nschneble@users.noreply.github.com>
2026-04-12 23:52:18 -0400

    GraphQL CSRF hardening

--- config/routes/graphql.rb
+++ config/routes/graphql.rb
@@ -1,6 +1,8 @@
 # a fresh, new GraphQL server designed for Rails
 
-get "/graphiql", to: "graphql#graphiql"
-get "/graphql/describe", to: "graphql#describe"
+if Rails.env.development?
+  get "/graphiql", to: "graphql#graphiql"
+  get "/graphql/describe", to: "graphql#describe"
+end
 
 post "/graphql", to: "graphql#execute"
