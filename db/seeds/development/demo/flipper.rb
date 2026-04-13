d6225692d20d2dc0024699613a411aa009065bfb
Nick Schneble <nschneble@users.noreply.github.com>
2026-04-13 14:50:55 -0400

    fix Flipper db reset bug

--- db/seeds/development/demo/flipper.rb
+++ db/seeds/development/demo/flipper.rb
@@ -1,5 +1,8 @@
 user  = User.find_or_create_by!(email: "user@superstack.dev", role: :user)
 admin = User.find_or_create_by!(email: "admin@superstack.dev", role: :admin)
 
-Flipper.enable_actor(:secrets, user)
-Flipper.enable_actor(:secrets, admin)
+Flipper::Adapters::Strict.with_sync_mode do
+  Flipper.add("secrets")
+  Flipper.enable_actor(:secrets, user)
+  Flipper.enable_actor(:secrets, admin)
+end
